ee9dac1a0ca2be5d8f7278e196a552c121f6ce67
Ibrahim-Ghazaly <ibrahimghazaly88@gmail.com>
2023-12-08 11:08:11 +0200

    change port

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.all("*",(req,res,next)=>
 app.use(globalError)
 
 // Server 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 const server = app.listen(port,()=>{
     console.log(`app running on port ${port}`)
 })
